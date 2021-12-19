a45ebc81d6d10199c7811ee7c3c38b16dfe86699
Tomisin Owolabi <48669572+Teetomania@users.noreply.github.com>
2021-12-19 10:47:38 +0100

    feedback's ui ready

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,27 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import FeedbackList from './components/FeedbackList.svelte';
+  let feedback = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id dapibus augue, scelerisque sodales dui. Quisque sodales facilisis fringilla. Quisque sed placerat urna. Donec id magna in dolor cursus tristique a vel massa. Fusce gravida, urna sit amet sollicitudin sodales, magna odio hendrerit nisi, vel blandit felis nulla non quam. Nam eget hendrerit quam. Aenean dictum arcu ac egestas venenatis. Proin mollis nibh lectus, nec tristique dolor laoreet efficitur. Maecenas vitae vehicula eros. Nulla accumsan, mauris et consequat fringilla, diam felis tristique dui, vel dapibus mauris velit eu ligula. Donec at vestibulum lorem. Phasellus et vestibulum leo. Fusce rhoncus ultrices justo eu fringilla. Nulla pretium odio vel leo porttitor tempus. Nullam rutrum aliquam mauris a tempor. Duis pulvinar augue sapien, ultricies gravida leo feugiat eu.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id dapibus augue, scelerisque sodales dui. Quisque sodales facilisis fringilla. Quisque sed placerat urna. Donec id magna in dolor cursus tristique a vel massa. Fusce gravida, urna sit amet sollicitudin sodales, magna odio hendrerit nisi, vel blandit felis nulla non quam. Nam eget hendrerit quam. Aenean dictum arcu ac egestas venenatis. Proin mollis nibh lectus, nec tristique dolor laoreet efficitur. Maecenas vitae vehicula eros. Nulla accumsan, mauris et consequat fringilla, diam felis tristique dui, vel dapibus mauris velit eu ligula. Donec at vestibulum lorem. Phasellus et vestibulum leo. Fusce rhoncus ultrices justo eu fringilla. Nulla pretium odio vel leo porttitor tempus. Nullam rutrum aliquam mauris a tempor. Duis pulvinar augue sapien, ultricies gravida leo feugiat eu.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id dapibus augue, scelerisque sodales dui. Quisque sodales facilisis fringilla. Quisque sed placerat urna. Donec id magna in dolor cursus tristique a vel massa. Fusce gravida, urna sit amet sollicitudin sodales, magna odio hendrerit nisi, vel blandit felis nulla non quam. Nam eget hendrerit quam. Aenean dictum arcu ac egestas venenatis. Proin mollis nibh lectus, nec tristique dolor laoreet efficitur. Maecenas vitae vehicula eros. Nulla accumsan, mauris et consequat fringilla, diam felis tristique dui, vel dapibus mauris velit eu ligula. Donec at vestibulum lorem. Phasellus et vestibulum leo. Fusce rhoncus ultrices justo eu fringilla. Nulla pretium odio vel leo porttitor tempus. Nullam rutrum aliquam mauris a tempor. Duis pulvinar augue sapien, ultricies gravida leo feugiat eu.',
+    },
+  ];
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <FeedbackList {feedback} />
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
